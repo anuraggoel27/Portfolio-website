a3acbf75b14ca9aa95b3c0e9610f374bb9a7b590
Anurag Goel <b119011@iiit-bh.ac.in>
2021-09-12 12:17:56 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Navbar} from "./"
+const Header = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Header
